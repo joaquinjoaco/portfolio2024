703226dae9f8668d5b3ae9c163ebde6ede720d97
Joaquín <joaquingomezleites@gmail.com>
2024-03-18 11:29:03 -0300

    Better styles for linkedin & github links
    And a small redaction tweak for the about me section

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { Github, Linkedin } from "lucide-react";
+import { Download, ExternalLink, Github, Linkedin } from "lucide-react";
 import Link from "next/link";
 
 const About = ({
@@ -17,45 +17,67 @@ const About = ({
             </h1>
             <p
                 className="font-sans text-base md:text-xl tracking-tight text-pretty">
-                I graduated from Technological Secondary Education in Computer Science. I&apos;m currently pursuing a technical degree in such field.
+                I graduated from Technological Secondary Education in Computer Science. I am currently pursuing a technical degree in such field.
 
-                One of the things I love about this industry is the opportunity to be creative and come up with new ideas, that can positively impact both the customer and the team.
+                One of the things I love about this industry is the opportunity to be creative and come up with new, great ideas, opening the door to explore new solutions that can positively impact both the customer and the team.
                 <br />
                 <br />
-                Over the past three years, I&apos;ve been self-educating myself in the fields of front-end development and both User Experience and User Interface design.
+                I am constantly evolving in the fields of web development and both User Experience and User Interface design.
 
-                I&apos;m currently stepping into Full Stack development working with a range of tools and technologies such as NextJS,
+                I am currently working with a range of tools and technologies such as NextJS,
                 React / React Native, Javascript, Typescript, HTML, CSS, MySQL, PostgreSQL, Prisma, TablePlus,
                 Firebase, Github, Figma.
                 <br />
-                I&apos;ve also used VueJS, Angular, VB .NET, C/C++ and Java.
+                I have also used VueJS, Angular, VB .NET, C/C++ and Java.
                 <br />
                 <br />
-                Check out my Linkedin & GitHub!
+                {/* Check out my Linkedin & GitHub!
                 <Link
                     href={"https://drive.google.com/file/d/1z3wG_oBuzhRWHJNweGId3dgk9AtOx8xK/view"}
                     target="_blank"
-                    className=" text-sky-200"
+                    className="text-sky-600 dark:text-sky-300 "
                 > (You can also download my resume)
-                </Link>
+                </Link> */}
             </p>
-            <div className="flex gap-x-4 pt-4">
-                <Link
-                    href={"https://www.linkedin.com/in/joaquingomezleites/"}
-                    target="_blank"
-                    title="My LinkedIn"
-                    className="p-2 rounded-2xl hover:bg-foreground hover:text-background transition-colors"
-                >
-                    <Linkedin className="h-8 w-8" />
-                </Link>
-                <Link
-                    href={"https://github.com/joaquinjoaco"}
-                    target="_blank"
-                    title="My GitHub"
-                    className="p-2 rounded-2xl hover:bg-foreground hover:text-background transition-colors"
-                >
-                    <Github className="h-8 w-8" />
-                </Link>
+            <div className="flex flex-col">
+                <div>
+                    Check out my Linkedin & GitHub!
+                    <Link
+                        href={"https://drive.google.com/file/d/1z3wG_oBuzhRWHJNweGId3dgk9AtOx8xK/view"}
+                        target="_blank"
+                        className="flex items-center text-sky-600 dark:text-sky-300"
+                        title="Google Drive"
+                    ><ExternalLink className="h-4 w-4 mr-2" />You can also download/view my resume
+                    </Link>
+                </div>
+                <div className="flex gap-x-4 pt-4">
+                    <Link
+                        href={"https://www.linkedin.com/in/joaquingomezleites/"}
+                        target="_blank"
+                        title="My LinkedIn"
+                        className="p-4 rounded-2xl border hover:bg-foreground hover:text-background transition-colors"
+                    >
+                        <div>
+                            <Linkedin className="h-8 w-8" />
+                            <p className="text-muted-foreground text-sm">
+                                joaquingomezleites
+                            </p>
+                        </div>
+                    </Link>
+                    <Link
+                        href={"https://github.com/joaquinjoaco"}
+                        target="_blank"
+                        title="My GitHub"
+                        className="p-4 rounded-2xl border hover:bg-foreground hover:text-background transition-colors"
+                    >
+                        <div>
+                            <Github className="h-8 w-8" />
+                            <p className="text-muted-foreground text-sm">
+                                joaquinjoaco
+                            </p>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
     );
